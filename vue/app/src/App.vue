c1c3b844ad0cd428bea04731ec50131ccccf4035
eunbinoh <nohonbi01@gmail.com>
2022-09-22 19:55:20 +0900

    vue project inject, multiple component create

---
+++ vue/app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Multiple></Multiple>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Multiple from './components/Multiple.vue';
+
+export default {
+    name: "App",
+    components: [
+        HelloWorld ,
+        Multiple
+    ],
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/app/src/components/Multiple.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <h1>끝말잇기</h1>
+  <div id="root">
+      <div>{{word}}</div>
+      <form @submit="onSubmitForm">
+          <input type="text" ref="answer" v-model="value">
+          <button type="submit">입력!</button>
+      </form>
+      <div>{{result}}</div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+
+const word = ref()
+const result = ref()
+const value = ref()
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  if (this.word[this.word.length - 1] === this.value[0]) {
+    this.result = '딩동댕';
+    this.word = this.value;
+    this.value = '';
+    this.$refs.answer.focus();
+  } else {
+    this.result = '땡';
+    this.value = '';
+    this.$refs.answer.focus();
+  }
+}
+
+</script>
+
+<style>
+
+</style>
